5c40d5321da0bf73f22be7f9d4e1636ce9658e1c
Cesar312 <cesarcmartinez@gmail.com>
2023-12-14 14:20:14 -0600

    Add udemy directory and its content

---
+++ udemy/exercises/exercise-00.go
@@ -0,0 +1,23 @@
+// exercise to print emoji and raw string literal
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("🍕")
+
+	fmt.Println(`
+	Lorem Ipsum
+
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+	dolore magna aliqua. Egestas dui id ornare arcu odio ut sem nulla pharetra. Vitae suscipit tellus mauris 
+	a diam maecenas sed enim. Donec massa sapien faucibus et. Accumsan sit amet nulla facilisi morbi tempus iaculis 
+	urna id. Et molestie ac feugiat sed lectus. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Facilisis 
+	leo vel fringilla est ullamcorper eget nulla facilisi. Nibh nisl condimentum id venenatis a. Et molestie ac feugiat 
+	sed. Maecenas volutpat blandit aliquam etiam erat velit scelerisque. Urna molestie at elementum eu facilisis. 
+	Turpis egestas integer eget aliquet nibh praesent tristique.
+	
+	`)
+}
