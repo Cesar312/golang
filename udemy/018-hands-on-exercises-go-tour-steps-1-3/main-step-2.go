d06a13965bff6b4d8ca2e5c3b5b402414bd2731d
Cesar312 <cesarcmartinez@gmail.com>
2023-12-15 17:19:22 -0600

    Add remaining exercise files

---
+++ udemy/018-hands-on-exercises-go-tour-steps-1-3/main-step-2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Print the square root of 8
+// %g is for large exponents
+func main() {
+	fmt.Println("") // Adding a newline
+	fmt.Printf("Now, you have a square root of 8, %g.\n\n", math.Sqrt(8))
+}
---
+++ udemy/018-hands-on-exercises-go-tour-steps-1-3/main-step-3.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Print pi
+
+func main() {
+	fmt.Println(math.Pi)
+}
