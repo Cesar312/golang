73a26312e9f1dc2d812eae06858ae9b813a8608b
Cesar312 <cesarcmartinez@gmail.com>
2023-12-15 18:24:20 -0600

    Add 020 directory

---
+++ udemy/020-hands-on-exercises-go-tour-steps-8-10/main-step-8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Var statements to declare a list of variables
+
+var c, python, java bool // list of variables as boolean
+
+func main() {
+	var i int // Note: i is set to 0 by default
+	fmt.Println(i, c, python, java)
+}
