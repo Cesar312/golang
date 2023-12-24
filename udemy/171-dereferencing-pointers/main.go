79b69e1c7a220e8f6ee46d2481a264ee350db15a
Cesar Martinez <cesarcmartinez@gmail.com>
2023-12-23 22:44:40 -0600

    Add 170, 171 directories; updated .gitignore

---
+++ udemy/171-dereferencing-pointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	fmt.Println(x)
+	fmt.Println(&x)
+
+	y := &x                        // Assign a pointer variable to the memory address of x
+	fmt.Printf("%v \t %T\n", y, y) // Print the value and type of the pointer variable y
+	fmt.Println(*y)                // Dereference the pointer variable
+	fmt.Println(*&x)               // Simply x (Note the staticcheck)
+
+	*y = 43        // Assigning a different value
+	fmt.Println(x) // Note the change to x
+	fmt.Println(*y)
+
+}
